dd2817cd84138bd6804df5abbcb102c408a94683
ThulinaWickramasinghe <thulinawickramasinghe@gmail.com>
2022-12-12 00:12:27 +0530

    Add project files.

---
+++ ShopOnline.Api/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShopOnline.Api.Repositories.Contracts;
+using ShopOnline.Models.Dtos;
+
+namespace ShopOnline.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRepository productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        public async Task<ActionResult<IEnumerable<ProductDto>>> GetItems()
+        {
+            try
+            {
+                var products = await productRepository.GetItems();
+                var productCategories = await productRepository.GetCategories();
+
+                if (null == products || null == productCategories)
+                {
+                    return NotFound();
+                }
+                else
+                {
+
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ ShopOnline.Api/Extensions/DtoConversions.cs
@@ -0,0 +1,12 @@
+﻿using ShopOnline.Models.Dtos;
+
+namespace ShopOnline.Api.Extensions
+{
+    public static class DtoConversions
+    {
+        public static IEnumerable<ProductDto> ConvertToDto()
+        {
+
+        }
+    }
+}
